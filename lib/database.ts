23f267d6fd2819ea0b5961594d484544f24d7464
clownnero <wlghd3351@naver.com>
2025-08-20 14:00:28 +0900

    Fix: 디비정보 수정

--- lib/database.ts
+++ lib/database.ts
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const dbConfig = {
-  host: process.env.DB_HOST || 'dev.timworks.co.kr',
-  user: process.env.DB_USER || 'biznavi',
-  password: process.env.DB_PASSWORD || 'Biz@1234!',
-  database: process.env.DB_NAME || 'BIZTLO',
+  host: process.env.DB_HOST || '',
+  user: process.env.DB_USER || '',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || '',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
